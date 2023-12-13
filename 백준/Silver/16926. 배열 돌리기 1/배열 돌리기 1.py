8c633592b102e1cfcbf9ecb8cc3c6521fc13c9e6
Park-Jaehyeon98 <78424655+Park-Jaehyeon98@users.noreply.github.com>
2023-12-13 20:58:09 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 548 ms, Memory: 112116 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -0,0 +1,36 @@
+def rotation(arr, n, m, cnt, r):
+    for i in range(r):
+        for j in range(cnt):
+            tmp = arr[j][j]
+            # 위
+            for k in range(j, m - j - 1):
+                arr[j][k] = arr[j][k+1]
+            # 오른쪽
+            for k in range(j, n - j - 1):
+                arr[k][m-j-1] = arr[k+1][m-j-1]
+            # 아래
+            for k in range(m - j - 1, j, -1):
+                arr[n-j-1][k] = arr[n-j-1][k-1]
+            # 왼쪽
+            for k in range(n - j - 1, j, -1):
+                arr[k][j] = arr[k-1][j]
+                
+            arr[j+1][j] = tmp
+    return arr
+
+n, m, r = map(int, input().split())
+
+arr = []
+
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    arr.append(tmp)
+
+cnt = min(n, m) // 2
+
+arr = rotation(arr, n, m, cnt, r)
+
+for i in range(len(arr)):
+    for j in range(len(arr[i])):
+        print(arr[i][j], end = " ")
+    print()
